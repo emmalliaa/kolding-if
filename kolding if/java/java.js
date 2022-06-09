4e3435c22b7d9bcee3c690629ca1accf03dbf52a
emmalliaa <98382867+emmalliaa@users.noreply.github.com>
2022-06-09 09:43:04 +0200

    Add files via upload

---
+++ kolding if/java/java.js
@@ -0,0 +1,14 @@
+const navbarToggle = () => {
+    const nav = document.querySelector('.nav'); 
+    nav.classList.toggle('toggleShow');
+    } 
+    document.querySelector('.fa-bars').addEventListener('click', navbarToggle);
+    window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+              if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("navbar").style.top = "0";
+              } else {
+                document.getElementById("navbar").style.top = "-50px";
+              }
+            }
